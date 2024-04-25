86a9ffd4baf0d6d64d1097813afa629636e2af23
nitta02 <nittaranjansarkertanmoy2022@gmail.com>
2024-04-25 22:31:28 +0600

    non commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,8 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:food_menu_app_ui/catagory_foodPages.dart/Italian_food.dart';
+import 'package:food_menu_app_ui/catagory_foodPages.dart/bengali_food.dart';
+import 'package:food_menu_app_ui/catagory_foodPages.dart/chines_food.dart';
+import 'package:food_menu_app_ui/catagory_foodPages.dart/indian_food.dart';
+import 'package:food_menu_app_ui/catagory_foodPages.dart/mexican_food.dart';
 import 'package:food_menu_app_ui/constants/color_constants.dart';
-import 'package:food_menu_app_ui/widgets/food_catagory.dart';
+import 'package:food_menu_app_ui/constants/constants_items.dart';
 import 'package:food_menu_app_ui/widgets/food_items.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -60,5 +65,68 @@ class HomeScreen extends StatelessWidget {
         )
       ],
     );
+  }
+}
+
+
+class FoodCatagory extends StatefulWidget {
+  const FoodCatagory({super.key});
+
+  @override
+  State<FoodCatagory> createState() => _FoodCatagoryState();
+}
+
+class _FoodCatagoryState extends State<FoodCatagory> {
+  int selectedIndex = 0;
+  List<Widget> foodPages = [
+    const FoodCatagory(),
+    const Indian_Food(),
+    const Mexican_Food(),
+    const Italian_Food(),
+    const ChinessFood(),
+    const Bengali_Food(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        padding: const EdgeInsets.all(10.0),
+        itemCount: countryFoodItems.length,
+        itemBuilder: (context, index) => _menuCatagory(index),
+      ),
+    );
+  }
+
+// ----------------------------------------------------------
+  Widget _menuCatagory(int index) {
+    return Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.0),
+        child: InkWell(
+          hoverColor: Colors.white,
+          onTap: () {
+            setState(() {
+              selectedIndex = index;
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(2.0),
+              // color: Colors.white,
+            ),
+            child: Center(
+              child: Text(
+                countryFoodItems[index],
+                style: TextStyle(
+                  letterSpacing: 2.1,
+                  fontWeight: selectedIndex == index
+                      ? FontWeight.bold
+                      : FontWeight.w400,
+                ),
+              ),
+            ),
+          ),
+        ));
   }
 }
